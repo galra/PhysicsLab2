6403cb90e06fbd4f05c074b68f8a7280c244f564
dore42 <kleinstern.dore@gmail.com>
2016-05-05 16:40:16 +0300

    plot+data

--- exp5/plot_graphs.m
+++ exp5/plot_graphs.m
@@ -1,8 +1,8 @@
 figure(1)
- semilogx(freq,I0_over_VG.*RT); 
+ semilogx(freq,I0_over_VG.*RT,'b'); 
  hold on
- semilogx(f,VRVG,'.')
+ semilogx(f,VRVG,'r.',[peak peak],[0 1],'g')
 figure(2)
- plot(freq,I0_over_VG.*RT); 
+ plot(freq,I0_over_VG.*RT,'b'); 
  hold on
- plot(f,VRVG,'.')
+ plot(f,VRVG,'r.',[peak peak],[0 1],'g')

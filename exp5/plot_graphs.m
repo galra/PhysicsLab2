6fbb82b9d64dc4a186c0041f3225185777112920
galra <galraay@gmail.com>
2016-05-06 17:46:32 +0300

    final graphs & code

--- exp5/plot_graphs.m
+++ exp5/plot_graphs.m
@@ -1,8 +1,22 @@
+I0_over_VG_gen = @(f) 1/(RT*sqrt(1+Q^2*(f/f0 - f0/f)^2));
+I0_over_VG = vertcat(arrayfun(I0_over_VG_gen, calc_theo_freq ));
+
 figure(1)
- semilogx(freq,I0_over_VG.*RT,'b'); 
+ semilogx(calc_theo_freq , I0_over_VG, 'b'); 
  hold on
- semilogx(f,VRVG,'r.',[peak peak],[0 1],'g')
+ semilogx(f,VRVG ./ R,'r.',[peak peak],[0 vert_line_height],'g')
+ title('Circuit''s frequency resonance (x-log scale)');
+ ylabel('I0/VG [Ohm]');
+ xlabel('f [Hz]');
+ legend('Theoretical curve for the system', 'Measured values', ...
+     'theoretical f0 (resonance freq.)', 'Location', 'NorthWest');
+ print(sprintf('exp%d_log_plot.png', exp_num), '-dpng');
 figure(2)
- plot(freq,I0_over_VG.*RT,'b'); 
+ plot(calc_theo_freq ,I0_over_VG,'b'); 
  hold on
- plot(f,VRVG,'r.',[peak peak],[0 1],'g')
+ plot(f,VRVG ./ R,'r.',[peak peak],[0 vert_line_height],'g')
+ title('Circuit''s frequency resonance');
+ ylabel('I0/VG [Ohm]');
+ xlabel('f [Hz]');
+ legend('Theoretical curve for the system', 'Measured values', 'theoretical f0 (resonance freq.)');
+ print(sprintf('exp%d_plot.png', exp_num), '-dpng');
